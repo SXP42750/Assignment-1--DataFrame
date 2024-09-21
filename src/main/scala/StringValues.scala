8c66eb0a6fe5ab094641df820adeb2a26c8cc10c
saika <sxp42750@ucmo.edu>
2024-09-20 21:15:54 -0400

    SampleQuestion4

---
+++ src/main/scala/StringValues.scala
@@ -0,0 +1,26 @@
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, when}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object StringValues {
+  def main(args: Array[String]): Unit = {
+    val sparkconf = new SparkConf()
+    sparkconf.set("spark.app.Name", "data")
+    sparkconf.set("spark.master", "local[*]")
+
+    val spark = SparkSession.builder()
+      .config(sparkconf)
+      .getOrCreate()
+
+    import spark.implicits._
+
+    val products = List(
+      (1, 30.5),
+      (2, 150.75),
+      (3, 75.25)
+    ).toDF("product_id", "price")
+
+    products.withColumn("price_range", when(col("price") < 50, "Cheap").when(col("price") > 50 && col("price") < 100, "Moderate").otherwise("Expensive")
+    ).show()
+  }
+}
